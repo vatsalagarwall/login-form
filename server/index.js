a752ccb9afe48085cbbb190feb2c3b19a0769606
Vatsal Agarwal <bindalvatsaljpr@gmail.com>
2023-12-24 18:30:40 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/login-register-employees")
 
 
 app.get("/", (req, res) => {
-    res.get("Hello")
+    res.json("Hello")
 })
 
 app.post("/login", (req, res) => {
